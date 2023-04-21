2f91b185e3eacc6529cf5584dec33db8c005ba3e
dotjalil <mgkmohammad@gmail.com>
2023-04-21 05:15:23 +0200

    Added favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
         <title>Mo. J. - Full Stack JavaScript Developer</title>
         <meta name="description" content="Portfolio website for Mo. J." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <main>
         <Header />
