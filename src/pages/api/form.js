f6e70d97a7c6fbac6aa9422c347a37d49f726940
dotjalil <mgkmohammad@gmail.com>
2023-04-25 00:09:22 +0200

    Changed mailer port

--- src/pages/api/form.js
+++ src/pages/api/form.js
@@ -6,7 +6,7 @@ async function sendMailData(name, email, msg) {
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: "smtppro.zoho.com",
-    port: 587,
+    port: 465,
     secure: false, // true for 465, false for other ports
     auth: {
       user: process.env.SMTP_USER, // generated ethereal user
