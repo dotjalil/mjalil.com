cb7cb62145f250ada5b09802c058398e38e1d569
dotjalil <mgkmohammad@gmail.com>
2023-04-25 09:18:00 +0200

    added contact and cv to hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,8 @@
+import Link from "next/link";
+import Image from "next/image";
+import downloadIcon from "../../public/download.svg";
+import emailIcon from "../../public/email.svg";
+
 const Hero = () => {
   return (
     <section id="hero-section" className="flex flex-col gap-6 lg:gap-16 py-28">
@@ -28,6 +33,28 @@ const Hero = () => {
             d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3"
           />
         </svg>
+      </div>
+      <div className="flex gap-7">
+        <Link
+          className="w-[198px] h-16 flex items-center justify-center h-full py-5 bg-[#E3E3E3] text-[#3A3A3A] border rounded-lg border-gray-200 text-base gap-4"
+          target="_blank"
+          href="https://drive.google.com/file/d/1xUhJ6FxSVxMa5AAD5rkZkVR-eVv2EM-R/view?usp=sharing"
+        >
+          Download CV
+          <Image
+            src={downloadIcon}
+            alt="download icon"
+            width={24}
+            height={24}
+          />
+        </Link>
+        <Link
+          className="w-[198px] h-16 flex items-center justify-center h-full py-5 bg-[#3A3A3A] text-[#E3E3E3] border rounded-lg border-[#3A3A3A] text-base gap-4"
+          href="mailto:mo@mjalil.com"
+        >
+          Contact Me
+          <Image src={emailIcon} alt="download icon" width={24} height={24} />
+        </Link>
       </div>
     </section>
   );
